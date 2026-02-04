52c9b8b62c14719c30a19c9036f2bcf030d998f0
WKDVP <jangkc1015@gmail.com>
2026-02-05 03:08:03 +0900

    g

--- app.py
+++ app.py
@@ -37,31 +37,59 @@ def upload_zip():
         zip_ref.extractall(extract_path)
 
     output_pdf = os.path.join(OUTPUT_DIR, "result.pdf")
+    if os.path.exists(output_pdf):
+        os.remove(output_pdf)
     jpg_images_to_pdf(extract_path, output_pdf)
     for d in [UPLOAD_DIR, EXTRACT_DIR]:
         for f in os.listdir(d):
             f_path = os.path.join(d, f)
             if os.path.isfile(f_path):
+                #print()
                 os.remove(f_path)
             else:
+                #print()
                 shutil.rmtree(f_path)
-
-    return send_file(output_pdf, as_attachment=True)
+    if os.path.exists(output_pdf):
+        return send_file(output_pdf, as_attachment=True)
+    else:
+        return "no return", 400
+    
 
 def jpg_images_to_pdf(input_folder, output_pdf):
-    image_files = [f for f in os.listdir(input_folder) if f.lower().endswith('.jpg') or f.lower().endswith('.jpeg') or f.lower().endswith('.png') or f.lower().endswith('.webp')]
+    image_files = [f for f in os.listdir(input_folder) if f.lower().endswith(('.jpg', '.jpeg', '.png', '.webp'))]
     image_files.sort()
-
+    if not image_files:
+        print("no img_files")
     images = []
     for file in image_files:
         img_path = os.path.join(input_folder, file)
         with Image.open(img_path) as img:
             img = img.convert("RGB")
-            images.append(img)
+            images.append(img.copy())
 
     if not images:
         print("이미지가 없습니다.")
         return
+    images[0].save(output_pdf, save_all=True, append_images=images[1:])
+    print(f"PDF 저장 완료: {output_pdf}")
+
+def png_images_to_pdf(input_folder, output_pdf):
+    # PNG 파일 목록 불러오기
+    image_files = [f for f in os.listdir(input_folder) if f.lower().endswith('.png')]
+    image_files.sort()  # 이름순 정렬
+
+    images = []
+    for file in image_files:
+        img_path = os.path.join(input_folder, file)
+        with Image.open(img_path) as img:
+            img_rgb = img.convert("RGB")
+            images.append(img_rgb)
+
+    if not images:
+        print("PNG 이미지가 없습니다.")
+        return
+
+    # 첫 장 + 나머지 추가 저장
     images[0].save(output_pdf, save_all=True, append_images=images[1:])
     print(f"PDF 저장 완료: {output_pdf}")
 
